ee12823a5fcc924996cf4f389ac549a89a352f9d
Hira Iqbal <hirach502@gmail.com>
2021-11-18 16:26:34 +0500

    moving form letter generation to a method

--- lib/google_api2.rb
+++ lib/google_api2.rb
@@ -24,6 +24,15 @@ def legislators_by_zipcode(zip)
   end
 end
 
+def save_thank_you_letter(id,form_letter)
+  Dir.mkdir('output') unless Dir.exist?('output')
+  filename = "output/thanks_#{id}.html"
+
+  File.open(filename, 'w') do |file|
+    file.puts form_letter
+  end
+end
+
 puts 'EventManager initialized.'
 
 contents = CSV.open(
@@ -36,6 +45,7 @@ template_letter = File.read('form_letter.erb')
 erb_template = ERB.new template_letter 
 
 contents.each do |row|
+  id = row[0]
   name = row[:first_name]
 
   zipcode = clean_zipcode(row[:zipcode])
@@ -43,6 +53,5 @@ contents.each do |row|
   legislators = legislators_by_zipcode(zipcode)
 
   form_letter = erb_template.result(binding)
-  puts form_letter
-
+  save_thank_you_letter(id,form_letter)
 end
